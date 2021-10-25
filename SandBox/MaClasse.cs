d2749eaa7127d05a0c8463f83bbcadf7f09f8117
val <valpruliere@gmail.com>
2021-10-25 20:28:05 +0200

    added first class

---
+++ SandBox/MaClasse.cs
@@ -0,0 +1,47 @@
+﻿namespace SandBox
+{
+    public class MaClasse
+    {
+        #region attributes
+
+        private string _tata;
+
+        #endregion
+
+        #region Properties (getters & setters)
+
+        public string GetTata()
+        {
+            return _tata;
+        }
+
+        public void SetTata(string tata)
+        {
+            _tata = tata;
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public MaClasse()
+        {
+        }
+        
+        public MaClasse(string parametre)
+        {
+            _tata = parametre;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void DoubleWord()
+        {
+            _tata += _tata;
+        }
+
+        #endregion
+    }
+}
--- SandBox/Program.cs
+++ SandBox/Program.cs
@@ -7,6 +7,14 @@ namespace SandBox
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var m = new MaClasse();
+            var m2 = new MaClasse("I am m2");
+            Console.WriteLine("a - m : " + m.GetTata());
+            Console.WriteLine("b - m2 : " + m2.GetTata());
+            m.SetTata("untruc");
+            Console.WriteLine("c - m : " + m.GetTata());
+            m.DoubleWord();
+            Console.WriteLine("d - m : " + m.GetTata());
         }
     }
 }
